701076e11fad489107281dfb199a97295a22daef
junad <qkrwnsgk1624@naver.com>
2023-01-30 20:08:36 +0900

    feat: create server
    create server
    - serve react app

---
+++ server/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const config = require("./src/config");
+const loaders = require('./src/loaders')
+
+const startServer = async () => {
+    const app = express();
+
+    await loaders( app );
+    app.listen(config.port);
+    app.on('error', onError);
+    app.on('listening', onListening);
+
+}
+
+const onError = (error) => {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+  
+    var bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port;
+  
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+}
+
+const onListening = () => {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
+  
+startServer();
