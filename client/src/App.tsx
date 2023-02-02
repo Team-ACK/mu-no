a491333c9e6a409dba033d9b1bed8f6784d3ff41
Seol_JY <wlsdud5654@gmail.com>
2023-02-02 21:39:24 +0900

    feat: setup styled-Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle, theme } from "./styles";
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>하이</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div className="App">
+        <h1>안녕하세요</h1>
+      </div>
+    </ThemeProvider>
   );
 };
-
 export default App;
