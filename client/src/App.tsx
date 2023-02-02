a2101e94ba09149761889bae70d1ba499e68499d
마재성 <majesoung@naver.com>
2023-02-02 23:33:28 +0900

    feat: setup react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle, theme } from "./styles";
+import { Home, Lobby } from "./pages";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div className="App">
-        <h1>안녕하세요</h1>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/lobby" element={<Lobby/>} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
