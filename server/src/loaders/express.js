5a2f736ddaea26d2e2e3de3da80baeed8a3ecbf1
junad <qkrwnsgk1624@naver.com>
2023-02-05 03:17:55 +0900

    chore: set cors
    install package
    - add cors

--- server/src/loaders/express.js
+++ server/src/loaders/express.js
@@ -1,9 +1,10 @@
 const path = require("path");
 const routes = require("../routes");
 const express = require("express");
-
+const cors = require("cors");
 
 module.exports = ( app ) => {
     app.use(express.static(path.join(__dirname, '../../../client/build')));
+    app.use(cors());
     app.use(routes());
 };
