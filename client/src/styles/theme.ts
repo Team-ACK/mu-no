0ed3caf95236faa91ce83f08776567729a2c9214
Seol_JY <wlsdud5654@gmail.com>
2023-02-05 02:10:48 +0900

    design: modify common style

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -2,12 +2,27 @@ const theme = {
   palette: {
     white: "#ffffff",
     black: "#393a43",
-    lightgray: '#fafafa',
+    lightgray: "#fafafa",
+    shadowColor: "rgba(0, 0, 0, 0.11)",
+    borderColor: "#e2e2e254",
   },
+
   typography: {
     caption: {
       fontFamily: '"Noto Sans KR", sans-serif',
-      fontSize: '0.875rem',
+      fontSize: "0.875rem",
+    },
+    button: {
+      fontFamily: '"Noto Sans KR", sans-serif',
+      fontSize: "20px",
+    },
+    input: {
+      fontFamily: '"Noto Sans KR", sans-serif',
+      fontSize: "27px",
+    },
+    information: {
+      fontFamily: '"Noto Sans KR", sans-serif',
+      fontSize: "24px",
     },
   },
 };
