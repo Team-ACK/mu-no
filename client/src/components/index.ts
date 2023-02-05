871aa52721bfb353677373299623f6d3f261315a
Seol_JY <wlsdud5654@gmail.com>
2023-02-06 02:45:40 +0900

    feat: add Profile Component

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Container } from "./Container";
 export { default as TextField } from "./TextField";
 export { default as Button } from "./Button";
+export { default as Profile } from "./Profile";
