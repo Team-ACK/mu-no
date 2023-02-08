fccca1b29d7775e51e2d6def5766adecab284832
guntak <imbmi4@naver.com>
2023-02-08 18:57:51 +0900

    refactor: seperate fucntion
    seperate getUserList function
    - from join-room socket event handler

--- server/src/services/socket.js
+++ server/src/services/socket.js
@@ -1,10 +1,31 @@
 module.exports = (io) => {
     let roomList = [];
 
+    const getUserList = (roomID) => {
+        const room = io.sockets.adapter.rooms.get(roomID);
+        const users = [];
+
+        try {
+            for (const item of io.sockets.sockets) {
+                if (room.has(item[0])) {
+                    let userInfo = {
+                        admin: item[1].admin,
+                        nickname: item[1].nickname,
+                        userColor: item[1].userColor,
+                    };
+                    users.push(userInfo);
+                }
+            }
+        } catch {
+            console.log("Can not find matching room");
+        }
+        return users;
+    };
+
     io.on("connection", (socket) => {
         socket.on("create-room", (data, done) => {
             const { nickname, userColor } = data;
-            const roomID = new Date().getTime().toString(36);
+            const roomID = new Date().getTime().toString(36); // 프런트 쪽에서 roomID다시 날려주는거 구현되면 이거
 
             socket.admin = true;
             socket.nickname = nickname;
@@ -12,6 +33,7 @@ module.exports = (io) => {
 
             socket.join(roomID);
             roomList.push(roomID);
+
             done(roomID);
         });
 
@@ -23,28 +45,13 @@ module.exports = (io) => {
             socket.nickname = nickname;
             socket.userColor = userColor;
 
-            if (isValidRoom) socket.join(roomID);
-
-            done(isValidRoom);
-        });
-
-        socket.on("user-list", (data, done) => {
-            const { roomID } = data;
-            const room = io.sockets.adapter.rooms.get(roomID);
-            const users = [];
-
-            for (const item of io.sockets.sockets) {
-                if (room.has(item[0])) {
-                    let userInfo = {
-                        admin: item[1].admin,
-                        nickname: item[1].nickname,
-                        userColor: item[1].userColor,
-                    };
-                    users.push(userInfo);
-                }
+            if (isValidRoom) {
+                socket.join(roomID);
+                const userList = getUserList(roomID);
+                io.to(roomID).emit("user-list", userList);
             }
 
-            done(users);
+            done(isValidRoom);
         });
     });
 };
