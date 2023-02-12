e827483fefeb12545aa7b86650ccca622e7f806c
Seol_JY <wlsdud5654@gmail.com>
2023-02-12 22:17:28 +0900

    feat: add isMe - move UserCard Component to public

--- client/src/components/UserCard.tsx
+++ client/src/components/UserCard.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
-import { ReactComponent as ProfileImg } from "../../../assets/img/profile_img.svg";
+import { ReactComponent as ProfileImg } from "../assets/img/profile_img.svg";
 
 type Props = {
   children: JSX.Element;
   profileColor: string;
   nickname: string;
+  isMe?: boolean;
 };
 
 const LayoutStyle = styled.div`
@@ -30,7 +31,7 @@ const S = {
   PlayerLayout: styled(LayoutStyle)`
     width: 93%;
     height: 90%;
-    border: 1px solid red;
+    border: 1px solid darkgray;
     border-radius: 100px 25px 25px 100px;
     margin: 10px 10px 5px 10px;
     flex-direction: row;
@@ -47,6 +48,7 @@ const S = {
   userNameLayout: styled(LayoutStyle)`
     align-items: flex-start;
     flex: 1 1 0%;
+    color: ${({ isMe }: { isMe?: boolean }) => (isMe ? "green" : "black")};
   `,
   userAuthLayout: styled(LayoutStyle)`
     width: 50px;
@@ -55,20 +57,16 @@ const S = {
   `,
 };
 
-const UserCard: React.FC<Props> = ({ children, profileColor, nickname }: Props) => (
+const UserCard: React.FC<Props> = ({ children, profileColor, nickname, isMe }: Props) => (
   <S.Test>
     <S.PlayerLayout>
-
       <S.userImgLayout profileColor={profileColor}>
         <ProfileImg width="50px" height="50px" fill="white" />
       </S.userImgLayout>
 
-      <S.userNameLayout>{nickname}</S.userNameLayout>
+      <S.userNameLayout isMe={isMe}>{nickname}</S.userNameLayout>
 
-      <S.userAuthLayout>
-        {children}
-      </S.userAuthLayout>
-
+      <S.userAuthLayout>{children}</S.userAuthLayout>
     </S.PlayerLayout>
   </S.Test>
 );
