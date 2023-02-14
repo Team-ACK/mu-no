ce2fd96b2b54a6f2d4dd644254d93d5fa5dac98c
Junad-Park <qkrwnsgk1624@icloud.com>
2023-02-14 16:32:59 +0900

    feat: create reaction game

--- server/src/models/room.js
+++ server/src/models/room.js
@@ -1,13 +1,32 @@
-class Room{
-    constructor(userList, isGaming=false){
+class Room {
+    constructor(userList, gameTitle = "undefined", isGaming = false) {
         this.userList = userList; // Array
+        this.gameTitle = gameTitle; // String
         this.isGaming = isGaming; // Boolean
-        this.gameResult = {}
-    };
-    
-    setUserList(userList){
+        this.targetResultCounts = this.userList.length;
+        this.gameResult = {};
+    }
+
+    setUserList(userList) {
         this.userList = userList;
     }
+
+    setTargetResultCounts(targetResultCounts) {
+        this.targetResultCounts = targetResultCounts;
+    }
+
+    getTargetResultCounts() {
+        return this.targetResultCounts;
+    }
+
+    setGameTitle(gameTitle) {
+        this.gameTitle = gameTitle;
+    }
+
+    setEmptyResult() {
+        this.gameResult[this.gameTitle] = [];
+    }
+
     // result Array for reaction test game (이건 나중에 뭐 게임마다 나누든 뭐든 어케든 해야함 ㅇㅇ)
     // 게임마다 이걸 나눌까요??
 
@@ -15,20 +34,21 @@ class Room{
     // UserList => Array
     // isGaming (게임 중이면 join 할 때 날려버려) => Boolean
     // 게임종류 : 반응속도테스트 혹은 마피아 혹은 라이어게임 => String?
-    
-    // setGameResult(gameTitle){
-    //     if (!(gameTitle in gameResult)){
-    //         gameResult[gameTitle] = [result]
 
-    //     }
-    //     else {
-    //         gameResult[gameTitle].push(result) // 결과
-    //     }
-    // }
-    
-    // getGameResult(){
-    //     return this.gameResult[gameTitle];
-    // }
+    setGameResult(data) {
+        const { socketID, speed } = data;
+        if (!(this.gameTitle in this.gameResult)) {
+            this.gameResult[this.gameTitle] = [{ socketID: socketID, speed: speed }];
+        } else {
+            this.gameResult[this.gameTitle].push({
+                socketID: socketID,
+                speed: speed,
+            });
+        }
+    }
+
+    getGameResult() {
+        return this.gameResult[this.gameTitle];
+    }
 }
-
 module.exports = Room;
