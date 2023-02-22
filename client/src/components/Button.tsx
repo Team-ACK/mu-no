af462b08718493fefbc316e3fefb5e1f344b4131
Seol_JY <wlsdud5654@gmail.com>
2023-02-23 01:01:10 +0900

    refactor: change components to be reusable

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,26 +1,54 @@
 import styled from "styled-components";
 
+interface StyledButtonType {
+  buttonType: "primary" | "warning";
+  disabled: boolean;
+}
+
+interface ButtonPropsType {
+  buttonType?: "primary" | "warning";
+  style?: React.CSSProperties;
+  disabled?: boolean;
+  children: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLElement>;
+}
+
+const buttonPalette = {
+  primary: {
+    disabledColor: (disabled: boolean) => (disabled ? "#b8baff" : "#696eff"),
+    hover: "#4d53fd",
+  },
+  warning: {
+    disabledColor: (disabled: boolean) => (disabled ? "#ff8989" : "#ff4040"),
+    hover: "#ff1d1d",
+  },
+};
+
 const S = {
-  Button: styled.button`
+  Button: styled.button<StyledButtonType>`
+    ${({ theme }) => theme.typography.button};
     margin: 7px;
     padding: 5px 20px;
     border: none;
     border-radius: 11px;
-    cursor: pointer;
-    ${(props) => props.theme.typography.button};
-    background-color: ${(props: any) => (props.red === "true" ? "#ff4040" : "#696eff")};
-    width: ${({ size }: { size: string | undefined }) => (typeof size === "string" ? size : "fit-content")};
+    cursor: ${({ disabled }) => (disabled ? "default" : "pointer")};
+    background-color: ${({ buttonType, disabled }) => buttonPalette[buttonType].disabledColor(disabled)};
+    width: fit-content;
     height: 46px;
     color: ${(props) => props.theme.palette.white};
     &:focus {
-      background-color: ${(props: any) => (props.red === "true" ? "#ff1d1d" : "#4d53fd")};
+      background-color: ${({ buttonType }) => buttonPalette[buttonType].hover};
     }
     &:hover {
-      background-color: ${(props: any) => (props.red === "true" ? "#ff1d1d" : "#4d53fd")};
+      background-color: ${({ buttonType }) => buttonPalette[buttonType].hover};
     }
   `,
 };
 
-const Button: React.FC<any> = (props) => <S.Button {...props} />;
+const Button = ({ children, style, onClick, buttonType = "primary", disabled = false }: ButtonPropsType) => (
+  <S.Button style={style} buttonType={buttonType} disabled={disabled} onClick={onClick}>
+    {children}
+  </S.Button>
+);
 
 export default Button;
